9a8ae78e0aaacd42144917d9bb5467f3d38c2bc2
josoomin <99530921+josoomin@users.noreply.github.com>
2022-11-21 13:45:27 +0900

    22

--- Assets/Basic/NetMob.cs
+++ Assets/Basic/NetMob.cs
@@ -5,21 +5,109 @@ using Mirror;
 
 public class NetMob : NetworkBehaviour
 {
+    public int _maxHp = 100;
+
+    [SyncVar]
+    public int _hp = 0;
+
+    SpriteRenderer _renderer;
+
     Vector3 _startPos;
+    Color _origColor; //original color
 
     void Start()
     {
-        GetComponent<SpriteRenderer>().sortingOrder = 1;
+        if(isServer)
+        {
+            //체력 초기화 (서버에서)
+            _hp = _maxHp;
+        }
+
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.sortingOrder = 1;
+
+        _origColor = _renderer.color;
 
         _startPos = transform.position;
     }
 
     void Update()
     {
-        Vector3 pos = transform.position;
+        ////서버에서 실행
+        //if(isServer)
+        //{
+        //    Debug.Log("Update Server : " + Time.time);
+        //}
 
-        pos.x = _startPos.x + Mathf.PingPong(Time.time, 2.0f);
+        //// 클라이언트에서 실행
+        //if(isClient)
+        //{
+        //    Debug.Log("Update Client : " + Time.time);
+        //}
 
-        transform.position = pos;
+        if (isServer)
+        {
+            Vector3 pos = transform.position;
+
+            pos.x = _startPos.x + Mathf.PingPong(Time.time, 2.0f);
+
+            transform.position = pos;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isServer)
+        {
+            _hp -= 10;
+
+            Debug.Log("Collision, attacker : " + collision.gameObject.name);
+
+            NetPlayer netPlayer = collision.gameObject.GetComponent<NetPlayer>();
+            if (netPlayer != null)
+            {
+                Debug.Log("attacker player name: " + netPlayer._playerName);
+            }
+
+            Rpc_DoDamageEffect();
+
+            if (_hp <= 0)
+            {
+                //사망처리
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    [ClientRpc]
+    void Rpc_DoDamageEffect()
+    {
+        StartCoroutine(_ChangeColor());
+    }
+
+    IEnumerator _ChangeColor()
+    {
+        _renderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+
+        _renderer.color = _origColor;
+
+        yield return new WaitForSeconds(0.1f);
+
+        _renderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+
+        _renderer.color = _origColor;
+
+        yield return new WaitForSeconds(0.1f);
+
+        _renderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+
+        _renderer.color = _origColor;
+
     }
 }
